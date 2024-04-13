c1aab22357a7d194ade202f6249d1a4636916ce1
MrX <Alihagh1383@gmail.com>
2024-04-13 03:46:22 -0400

    Episod 3

---
+++ Compile/Tokenizer.cs
@@ -0,0 +1,48 @@
+// Episod 3
+using MrX.DynamicCompiler.Static;
+
+namespace MrX.DynamicCompiler.Compile;
+
+public class Tokenizer
+{
+    public Tokenizer()
+    {
+        for (int i = 0; i < Statics.L_Splits.Count; i++)
+        {
+            string l = Statics.L_Splits[i];
+            Statics.LabalTypes find;
+
+            if ((l != string.Empty || l != null)
+                && l.Length > 2
+                && l[0] == '/'
+                && l[1] == '*'
+                && l[l.Length - 2] == '*'
+                && l[l.Length - 1] == '/'
+               )
+                find = Statics.LabalTypes.Comment;
+            else if (int.TryParse(l, out _))
+            {
+                find = Statics.LabalTypes.Number;
+            }
+            else
+            {
+                var finds = Statics.Labals.Where(P => P.Key == l).Count();
+                find = finds > 0
+                    ? Statics.Labals.First(P => P.Key == l).Value.Key
+                    : Statics.LabalTypes.Unknown;
+            }
+
+
+            if (i > 0
+                && Statics.L_Tokens.Last().Key == Statics.LabalTypes.EndOfCode
+                && find == Statics.LabalTypes.EndOfCode)
+            {
+            }
+            else
+            {
+                var Token = new KeyValuePair<Statics.LabalTypes, String>(find, l);
+                Statics.L_Tokens.Add(Token);
+            }
+        }
+    }
+}
